5d35e4899ee359427bb36bdc72e13f586011675e
nahid-sheikh <nahidsheikh.shariar.com>
2021-08-10 21:49:15 +0600

    added personal portfolio link in img

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,6 @@ import { RiArrowDownSFill } from "react-icons/ri";
 import { AiFillGithub } from "react-icons/ai";
 
 const Header = () => {
-  const [showProfile, setShowProfile] = useState(false);
-  console.log(showProfile);
   return (
     <header className="sticky top-0 flex justify-between py-8 px-8 md:px-16 bg-white z-50">
       {/* left  */}
@@ -19,41 +17,16 @@ const Header = () => {
         <a href="https://github.com/ShariarSheikh">
           <AiFillGithub className="w-9 h-9" />
         </a>
-        <div
+        <a
+          href="https://shariar.vercel.app/"
           className="flex justify-center items-center cursor-pointer space-x-2"
-          onClick={() => setShowProfile((prevState) => !prevState)}
         >
           <img
             className="object-cover w-9 h-9 rounded-full border-2 border-blue-500"
             src="https://media.istockphoto.com/photos/illustration-of-smiling-happy-man-with-laptop-sitting-in-armchair-picture-id1226886130?b=1&k=6&m=1226886130&s=170667a&w=0&h=9VyVuQvQfzzQaYB1iEfy-p5YaEe59lzeIe3BfkTOnBE="
             alt="person profile"
           />
-          <RiArrowDownSFill
-            className={`w-6 h-6 text-blue-500 cursor-pointer transition duration-150 ease-out ${
-              showProfile ? "transform rotate-180" : "transform rotate-0"
-            }`}
-          />
-        </div>
-        {/* profile drop down menu  */}
-        <div
-          className={`absolute top-10 shadow-xl ${
-            showProfile ? "inline-block" : "hidden"
-          } w-52 py-5 px-4 z-50 bg-white`}
-        >
-          <p className="cursor-pointer hover:text-blue-500 text-gray-600 text-sm font-semibold mb-2">
-            Profile
-          </p>
-          <p className="cursor-pointer hover:text-blue-500 text-gray-600 text-sm font-semibold mb-2">
-            Login
-          </p>
-          <p className="cursor-pointer hover:text-blue-500 text-gray-600 text-sm font-semibold mb-2">
-            Sign Up
-          </p>
-          <div className="h-1 w-full bg-gray-400 my-5" />
-          <p className="cursor-pointer hover:text-blue-500 text-gray-600 text-sm font-semibold">
-            Logout
-          </p>
-        </div>
+        </a>
       </div>
     </header>
   );
